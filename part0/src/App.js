c530dbe09885790ec94ecaa04146157942bd124b
preetam <preetammondal78@gmail.com>
2023-05-20 10:52:19 +0530

    component for rendering and prop-passing is done

--- part0/src/App.js
+++ part0/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
 
-function App() {
-  console.log("App component render")
+
+const Hello = (props) => {
+
+  const bornYear = () => {
+
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  }  
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+ <p>So you were probably born in {bornYear()}</p>
+
     </div>
-  );
+  )
+}
+
+function App() {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
 }
 
 export default App;
