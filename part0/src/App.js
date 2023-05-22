858729f1a137fa4c790084c950b14dbee19db768
preetam <preetammondal78@gmail.com>
2023-05-22 23:13:19 +0530

    added the code for the usestate to handle the state as object

--- part0/src/App.js
+++ part0/src/App.js
@@ -1,36 +1,84 @@
+import { useState } from 'react';
 import './App.css';
 
+// const Display = ({message}) => {
+//   return (
+//     <div>
+//       {message}
+//     </div>
+//   )
+// }
 
-const Hello = (props) => {
+// const Button = ({handleClick, message}) => {
+//   return (
+//     <button onClick={handleClick}>
+//       {message}
+//     </button>
+//   )
+// }
 
-  const bornYear = () => {
+// const App = () => {
+//   const [ counter, setCounter ] = useState(0)
 
-    const yearNow = new Date().getFullYear();
-    return yearNow - props.age;
-  }  
+//   const increaseByOne = () => {
+//     setCounter(counter+1);
+//   }
+
+//   const setCounterZero = () => {
+//     setCounter(0);
+//   }
+
+//   const decreaseByOne = () => {
+//     setCounter(counter - 1);
+//   }
+
+//   return (
+//     <>
+//     <Display message={counter}/>
+//     <Button handleClick={increaseByOne} message={'plus'}/>
+//     <Button handleClick={decreaseByOne} message={'minus'}/>
+//     <Button handleClick={setCounterZero} message={'zero'}/>
+//     </>
+//   )
+// }
+
+const App = () => {
+  // const [left, setLeft] = useState(0)
+  // const [right, setRight] = useState(0)
+
+  let InitailState = {
+    left:0,
+    right:0
+  };
+  const [click, setClick] = useState(InitailState);
+
+  const handleLeftClick = () => {
+    const newClick = {
+      ...click,
+      left:click.left+1
+    }
+    setClick(newClick);
+  }
+
+  const handleRightClick = () => {
+    const newClick = {
+      ...click,
+      right:click.right+1
+    }
+    setClick(newClick);
+  }
 
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
- <p>So you were probably born in {bornYear()}</p>
-
+      {click.left}
+      <button onClick={handleLeftClick}>
+        left
+      </button>
+      <button onClick={handleRightClick}>
+        right
+      </button>
+      {click.right}
     </div>
   )
 }
-
-function App() {
-  const name = 'Peter'
-  const age = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
-
-export default App;
+ export default App
